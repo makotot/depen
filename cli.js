32346a5ec242aae080361f0ee466ef8c850706a3
sfusco <sfusco@ricin.local>
2015-05-29 14:55:53 -0400

    recursive support with -r flag

--- cli.js
+++ cli.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
-var meow = require('meow'),
+var
+  fs = require('fs'),
+  meow = require('meow'),
   chalk = require('chalk'),
   figures = require('figures'),
   depen = require('./');
@@ -8,25 +10,38 @@ var meow = require('meow'),
 var cli = meow({
   help: [
     'Usage',
-    ' $ depen <project dir>'
+    ' $ depen [-r] [project dir]'
   ].join('\n')
 });
 
+function printDeps (root, indent, shiftwidth) {
 
+  depen(root, function (err, dependencies) {
+    if (err) {
+      console.log(err);
+    }
 
-depen(cli.input[0], function (err, dependencies) {
-  if (err) {
-    console.log(err);
-  }
+    dependencies.dependency.forEach(function (dep) {
+      var offset = Array(shiftwidth).join('  ');
 
-  dependencies.dependency.forEach(function (dep) {
-    console.log(figures.pointer, chalk.white.underline(dep.type));
+      console.log(offset + figures.pointer, chalk.white.underline(dep.type));
 
-    dep.package.forEach(function (package) {
-      console.log('   ', chalk.blue(package.name), chalk.yellow(package.version));
-      console.log('     ', chalk.gray(package.description ? package.description : '---'), chalk.magenta(package.homepage ? package.homepage : '---'));
+      dep.package.forEach(function (package) {
+        console.log(offset + indent, chalk.blue(package.name), chalk.yellow(package.version));
+        console.log(offset + indent + indent, chalk.gray(package.description ? package.description : '---'), chalk.magenta(package.homepage ? package.homepage : '---'));
+
+        if (cli.flags['r']) {
+          var innerpath = root + '/node_modules/' + package.name;
+          if (fs.existsSync(innerpath)) {
+            printDeps(innerpath, indent, shiftwidth + 2);
+          }
+        }
+
+      });
+      console.log('');
     });
-    console.log('');
   });
-});
 
+}
+
+printDeps(cli.input[0]||'.', '   ', 0);
